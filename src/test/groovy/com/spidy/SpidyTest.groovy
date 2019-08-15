0175db7a228e45c84e624561d7f8bdcac5af0620
aceccott <arthur.b.ceccotti@aexp.com>
2019-08-15 23:16:51 +0100

    Depth 2

--- src/test/groovy/com/spidy/SpidyTest.groovy
+++ src/test/groovy/com/spidy/SpidyTest.groovy
@@ -16,7 +16,7 @@ class SpidyTest extends Specification {
             new Page(root, []) == page
     }
 
-    def "Page with one link"() {
+    def "Page with one link, depth 1"() {
         given:
             def root = "index.html"
             def firstLink = "first.html"
@@ -33,7 +33,7 @@ class SpidyTest extends Specification {
             ]) == page
     }
 
-    def "Page with two links"() {
+    def "Page with two links, depth 1"() {
         given:
         def root = "index.html"
         def firstLink = "first.html"
@@ -41,8 +41,8 @@ class SpidyTest extends Specification {
 
         def spidy = new Spidey(Mock(WebConnector){
             1 * get(root) >> "<html>" +
-                    "<a href='${firstLink}'>First</a>" +
-                    "<a href='${secondLink}'>Second</a>" +
+                        "<b>Here is a link: <a href='${firstLink}'>First</a></b>" +
+                        "<p>Here is another link: <a href='${secondLink}'>Second</a></b>" +
                     "</html>"
             1 * get(firstLink) >> "<html></html>"
             1 * get(secondLink) >> "<html></html>"
@@ -53,6 +53,27 @@ class SpidyTest extends Specification {
         new Page(root, [
                 new Page(firstLink, []),
                 new Page(secondLink, [])
+        ]) == page
+    }
+
+    def "Page with one link, depth 2"() {
+        given:
+        def root = "index.html"
+        def firstLink = "first.html"
+        def firstFollowUpLink = "followup1.com"
+
+        def spidy = new Spidey(Mock(WebConnector){
+            1 * get(root) >> "<html><a href='${firstLink}'>First</a></html>"
+            1 * get(firstLink) >> "<html><a href='${firstFollowUpLink}'>Follow-up 1</a></html>"
+            1 * get(firstFollowUpLink) >> "<html></html>"
+        })
+        when:
+        def page = spidy.crawl(root)
+        then:
+        new Page(root, [
+                new Page(firstLink, [
+                        new Page(firstFollowUpLink, [])
+                ])
         ]) == page
     }
 
